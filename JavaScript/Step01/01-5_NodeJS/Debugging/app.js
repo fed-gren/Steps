093f8610bb2d10ccefc2a458e83c8a27b2d7f9d8
fed-gren <deveb22@gmail.com>
2019-04-05 15:44:09 +0900

    Add debugging practice
    디버깅 연습을 위한 파일 추가
    break point, log point, debug console REPL, etc

---
+++ JavaScript/Step01/01-5_NodeJS/Debugging/app.js
@@ -0,0 +1,7 @@
+let msg = "Hello World";
+
+for(let i=1; i<11; i+=1) {
+  console.log(`${i} 번째 실행입니다.`);
+}
+
+console.log("End");
---
+++ JavaScript/Step01/01-5_NodeJS/Debugging/bye.js
@@ -0,0 +1,9 @@
+let msg = "Good Bye!";
+
+console.log(`1 : ${msg}`);
+console.log(`2 : ${msg}`);
+console.log(`3 : ${msg}`);
+console.log(`4 : ${msg}`);
+console.log(`5 : ${msg}`);
+console.log(`6 : ${msg}`);
+console.log(`7 : ${msg}`);
