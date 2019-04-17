a2a63c339f02e2f2c231e72aeb6c6dfcec630f9d
fed-gren <deveb22@gmail.com>
2019-04-17 16:12:29 +0900

    quit가 입력될 때까지 명령어 입력하는 함수 executePrompt() 작성

--- CS23/Step03/app.js
+++ CS23/Step03/app.js
@@ -3,21 +3,29 @@ const readline = require("readline");
 const VMGit = require("./vmgit");
 const vmgit = new VMGit();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function executePrompt() {
-  rl.question("/> ", function(command) {
-    if (command === "quit") return rl.close();
-    log('Got it! Your command was: "', command, '"');
-    if (command === "init") vmgit.init();
-    executePrompt(); //Calling this function again to new command
-  });
-}
-
 const app = () => {
+  const vmgitFunctions = {
+    init: vmgit.init,
+    status: vmgit.status,
+    checkout: vmgit.checkout
+  };
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  function executePrompt() {
+    rl.question("/> ", function(command) {
+      if (command === "quit") return rl.close();
+      try {
+        vmgitFunctions[command]();
+      } catch (error) {
+        log(`입력한 명령어 ${command}는 지원하지 않습니다.`);
+      }
+      executePrompt(); //Calling this function again to new command
+    });
+  }
+
   executePrompt();
 };
 
