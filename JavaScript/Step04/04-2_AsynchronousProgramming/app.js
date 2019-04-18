01bc98ef03aa6bd701a95c879a8232756b418a8f
fed-gren <deveb22@gmail.com>
2019-04-18 15:11:05 +0900

    정규표현식 사용
    정규표현식을 사용하여 실행 명령어 판단
    객체 내 메소드에 [] 리터럴로 접근하는 방식 사용 -> 조건문 제거

--- JavaScript/Step04/04-2_AsynchronousProgramming/app.js
+++ JavaScript/Step04/04-2_AsynchronousProgramming/app.js
@@ -1,57 +1,37 @@
+const { log } = console;
 const todoList = require("./todo");
 const myTodoList = new todoList();
 
+const getCommand = () => {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-const getCommand = () =>{
-    const readline = require("readline");
-    const rl = readline.createInterface({
-        input : process.stdin,
-        output: process.stdout
-    });
-       
-    
-    rl.on("line", function(line){
+  rl.on("line", function(line) {
+    const commandArray = line.split("$");
+    executeCommand(commandArray);
+  }).on("close", function() {
+    process.exit();
+  });
+};
 
-        // rl.close();
-        const commandArray = line.split("$");
-        executeCommand(commandArray);
-    }).on("close", function() {
-        process.exit();
-    });
-}
-
-
-const executeCommand = (commandArray) => {
-    const action = commandArray[0];
-    if(action === "show"){
-        myTodoList.show(commandArray[1]);
-    }else if(action === "add"){
-        name = commandArray[1];  
-        tag = commandArray[2];
-        
-        myTodoList.add(name,tag);
-
-    }else if(action === "delete"){
-        id = parseInt(commandArray[1]);
-        myTodoList.deleteData(id);
-    
-    }else if(action === "update"){
-        id =  parseInt(commandArray[1]);
-        status = commandArray[2];
-
-        myTodoList.updateData(id,status);
-    }else{
-        console.log("명령어가 유효하지 않습니다.");
-    }
-}
+const executeCommand = commandArray => {
+  const action = commandArray[0];
+  commandArray.shift();
+  const commandParameters = commandArray;
+  const actionReg = /show|add|delete|update/;
+  let actionMatchResult = action.match(actionReg);
+  try {
+    myTodoList[`${actionMatchResult[0]}Data`](...commandParameters);
+  } catch (error) {
+    log(error);
+  }
+};
 
 const main = () => {
-    getCommand();
-}
+  getCommand();
+};
 
 main();
-
-// myTodoList.show("all");
-// myTodoList.show("todo");
-// myTodoList.show("doing");
-// myTodoList.show("done");
--- JavaScript/Step04/04-2_AsynchronousProgramming/todo.js
+++ JavaScript/Step04/04-2_AsynchronousProgramming/todo.js
@@ -1,3 +1,4 @@
+const { log } = console;
 const todos = [
   {
     name: "자바스크립트 공부하기",
@@ -37,15 +38,15 @@ const todos = [
   }
 ];
 
-const ids = [423,1535,9999,512,2,77];
+const ids = [423, 1535, 9999, 512, 2, 77];
 
 module.exports = class TodoList {
   count() {
     const countObj = {
-      todo : 0,
-      doing : 0,
-      done : 0
-    }
+      todo: 0,
+      doing: 0,
+      done: 0
+    };
 
     todos.forEach(element => {
       const { status } = element;
@@ -59,17 +60,17 @@ module.exports = class TodoList {
     resultStr = `현재상태 : todo : ${countObj.todo}개, doing : ${
       countObj.doing
     }개, done : ${countObj.done}개`;
-    console.log(resultStr);
+    log(resultStr);
   }
 
   getNames(status) {
     const namesArr = todos
-    .filter(function(element){
-      return status === element.status
-    })
-    .map(function(element){
-      return element.name;
-    })
+      .filter(function(element) {
+        return status === element.status;
+      })
+      .map(function(element) {
+        return element.name;
+      });
     return namesArr;
   }
 
@@ -81,10 +82,10 @@ module.exports = class TodoList {
         resultStr += `, `;
       }
     });
-    console.log(resultStr);
+    log(resultStr);
   }
 
-  show(status) {
+  showData(status) {
     if (status === "all") {
       const countObj = this.count();
       this.printAll(countObj);
@@ -92,71 +93,67 @@ module.exports = class TodoList {
       const namesArr = this.getNames(status);
       this.printList(namesArr, status);
     } else {
-      throw Error("show의 인자는 todo, doing, done, all 만 가능합니다!");
+      log("show의 인자는 todo, doing, done, all 만 가능합니다!");
     }
   }
 
-  add(name, tags) {
+  addData(name, tags) {
     let id;
-    while(true) {
-      id = Math.floor(Math.random() * 10000) +1;
-      if(ids.indexOf(id) === -1) {
+    while (true) {
+      id = Math.floor(Math.random() * 10000) + 1;
+      if (ids.indexOf(id) === -1) {
         ids.push(id);
         break;
       }
     }
 
     const todoObj = {
-      "name" : name,
-      "tags" : tags,
-      "status" : "todo",
-      "id" : id
-    }
+      name: name,
+      tags: tags,
+      status: "todo",
+      id: id
+    };
     todos.push(todoObj);
-    console.log(`${name} 1개가 추가됐습니다.(id : ${id})`);
+    log(`${name} 1개가 추가됐습니다.(id : ${id})`);
     setTimeout(() => {
-      this.show("all");
+      this.showData("all");
     }, 1000);
   }
 
-  deleteData(id){
+  deleteData(id) {
     let deletingObj;
+    const numId = parseInt(id);
 
     todos.forEach((element, index) => {
-      if(element.id === id) {
+      if (element.id === numId) {
         deletingObj = element;
         todos.splice(index, 1);
       }
     });
 
-    console.log(`${deletingObj.name}이(가) ${deletingObj.status} 목록에서 삭제됐습니다.`);
+    log(
+      `${deletingObj.name}이(가) ${deletingObj.status} 목록에서 삭제됐습니다.`
+    );
     setTimeout(() => {
-      this.show("all");
+      this.showData("all");
     }, 1000);
-  };
+  }
 
   updateData(id, status) {
     let name;
-
-    todos.forEach((element) =>{
-      if(element.id === id){
+    const numId = parseInt(id);
+    todos.forEach(element => {
+      if (element.id === numId) {
         element.status = status;
         name = element.name;
       }
     });
-    
+
     setTimeout(() => {
-      console.log(`${name}이(가) ${status}로 상태가 변경됐습니다.`)
+      log(`${name}이(가) ${status}로 상태가 변경됐습니다.`);
       setTimeout(() => {
-        this.show("all");
+        this.showData("all");
       }, 1000);
     }, 3000);
   }
-}
-
-// const myTodo = new TodoList();
-// myTodo.show("all");
-// myTodo.show("todo");
-// myTodo.show("doing");
-// myTodo.show("done");
-// myTodo.show("too");
+};
