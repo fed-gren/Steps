1134417e88406d0c63b9e1dbc9d5e134f3717b67
fed-gren <deveb22@gmail.com>
2019-04-24 15:59:41 +0900

    fix: sperate checkExistRepo method
    - 저장소가 local에 존재하는 지 확인하는 로직 함수화

--- CS23/Step03-1/fileManagement.js
+++ CS23/Step03-1/fileManagement.js
@@ -3,15 +3,21 @@ const fs = require("fs");
 const localPath = `${__dirname}/local`;
 const remotePath = `${__dirname}/remote`;
 
+const repoList = fs.readdirSync(localPath);
+
 //필요한 기능 : 파일 생성, 폴더 검색해서 문자열 리턴
+
+const checkExistRepo = repoName => {
+  let repoExsitFlag = false;
+  repoExsitFlag = repoList.some(repo => {
+    return repo === repoName;
+  });
+  return repoExsitFlag;
+};
 
 module.exports = FM = {
   initRepo(repoName) {
-    const repoList = fs.readdirSync(localPath);
-    let repoExsitFlag = false;
-    repoExsitFlag = repoList.some(repo => {
-      return repo === repoName;
-    });
+    const repoExsitFlag = checkExistRepo(repoName);
     if (repoExsitFlag) {
       console.log("저장소가 이미 존재합니다.");
       return;
