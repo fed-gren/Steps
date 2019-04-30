34f5b2afa9a1bab9646be29eebf923aee9cbcae8
fed-gren <deveb22@gmail.com>
2019-04-30 15:10:38 +0900

    feat: create isSeparator method
    - 구분자인지 파악하기 위한 함수 작성
    - 구분자들 모아놓은 객체 separator 작성

--- JavaScript/Step06/06-1_ArrayParser_Token/Parser.js
+++ JavaScript/Step06/06-1_ArrayParser_Token/Parser.js
@@ -1,15 +1,34 @@
+const literals = require("./literals");
+const separators = require("./separators");
+const print = console.log;
+
 class Parser {
   constructor(data) {
     this.data = data;
-    this.json = [];
+    this.lexedData = [];
   }
   lexer() {
     //데이터 모두 빠개버리기
-    this.json = this.data.split("");
+    this.lexedData = this.data.split("");
+    this.isSeparator();
+    this.lexedData.forEach((letter) => print(this.isSeparator(letter)));
+  }
+
+  tokenizer() {
+    //배열 돌면서 의미있는 데이터로 만들기
+    //'[', ']', 'number', ',' (나중엔 number 뿐만 아니라 다른 데이터 형도 인지해서 추가해야한다.(string, boolean, null))
+    // lexedData.
+  }
+
+  isSeparator(letter) {
+    for(let separator of Object.values(separators)) {
+      if(letter === separator) return true;
+    }
+    return false;
   }
 }
 
 const str = "[123, 22, 33]";
 const parser = new Parser(str);
 parser.lexer();
-console.log(parser.arr);
+print(parser.lexedData);
---
+++ JavaScript/Step06/06-1_ArrayParser_Token/separators.js
@@ -0,0 +1,5 @@
+module.exports = {
+  startOfArray: "[",
+  endOfArray: "]",
+  rest: ","
+};
