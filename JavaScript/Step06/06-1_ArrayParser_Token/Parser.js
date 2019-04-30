0712b44d382fcf00ac998f31eb9ec37088867603
fed-gren <deveb22@gmail.com>
2019-04-30 15:41:35 +0900

    feat: create joinLiterals method
    - letter 단위로 쪼갠 lexedData를 순회하면서 literal인 경우, 구분자가 나오기 전까지 합치는 함수

--- JavaScript/Step06/06-1_ArrayParser_Token/Parser.js
+++ JavaScript/Step06/06-1_ArrayParser_Token/Parser.js
@@ -6,18 +6,20 @@ class Parser {
   constructor(data) {
     this.data = data;
     this.lexedData = [];
+    this.tokenizedData = [];
   }
   lexer() {
     //데이터 모두 빠개버리기
     this.lexedData = this.data.split("");
     this.isSeparator();
-    this.lexedData.forEach((letter) => print(this.isSeparator(letter)));
+    // this.lexedData.forEach((letter) => print(this.isSeparator(letter)));
   }
 
   tokenizer() {
     //배열 돌면서 의미있는 데이터로 만들기
     //'[', ']', 'number', ',' (나중엔 number 뿐만 아니라 다른 데이터 형도 인지해서 추가해야한다.(string, boolean, null))
-    // lexedData.
+    //separator가 아닌 데이터는 다 합치기
+    print(this.joinLiterals());
   }
 
   isSeparator(letter) {
@@ -26,9 +28,28 @@ class Parser {
     }
     return false;
   }
+
+  joinLiterals() {
+    let data = "";
+    const literalsJoinedArray = this.lexedData.map((letter, idx) => {
+      if(this.isSeparator(letter)) {
+        data = "";
+        return letter;
+      } else {
+        data += letter;
+        if(idx < this.lexedData.length-1 && this.isSeparator(this.lexedData[idx + 1])) {
+          return data.trim();
+        }
+      }
+    }).filter((letter) => {
+      return letter !== undefined;
+    });
+    return literalsJoinedArray;
+  }
 }
 
 const str = "[123, 22, 33]";
 const parser = new Parser(str);
 parser.lexer();
 print(parser.lexedData);
+parser.tokenizer();
