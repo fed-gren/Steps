8d29056e8a0620100fc67ab519eec846d10b97b4
fed-gren <deveb22@gmail.com>
2019-04-30 16:44:54 +0900

    fix: exchange of tokenizer logics with lexer logic
    - tokenizer : 의미있는 단어들로 나누는 로직으로 수정
    - lexer : 각 데이터에 의미 부여

--- JavaScript/Step06/06-1_ArrayParser_Token/Parser.js
+++ JavaScript/Step06/06-1_ArrayParser_Token/Parser.js
@@ -8,39 +8,46 @@ class Parser {
     this.lexedData = [];
     this.tokenizedData = [];
   }
-  lexer() {
-    //데이터 모두 빠개버리기
-    this.lexedData = this.data.split("");
-    this.isSeparator();
-    // this.lexedData.forEach((letter) => print(this.isSeparator(letter)));
-  }
 
   tokenizer() {
+    //데이터 모두 빠개버리기
+    this.tokenizedData = this.data.split("");
+  }
+
+  lexer() {
     //배열 돌면서 의미있는 데이터로 만들기
     //'[', ']', 'number', ',' (나중엔 number 뿐만 아니라 다른 데이터 형도 인지해서 추가해야한다.(string, boolean, null))
     //separator가 아닌 데이터는 다 합치기
+
+    this.lexedData = this.data.split("");
+    this.isSeparator();
+    // this.lexedData.forEach((letter) => print(this.isSeparator(letter)));
+    const token = {};
     print(this.joinLiterals());
     const joinedLiteralData = this.joinLiterals();
-    joinedLiteralData.forEach((word) => {
-      if(!this.isSeparator(word)) {
-        print(this.getLiteralsType(word));
+    joinedLiteralData.forEach(word => {
+      if (!this.isSeparator(word)) {
+        token.type = this.getLiteralsType(word);
+        token.value = word;
+        token.child = []; //array인 경우 추가
+        print(token);
       }
     });
   }
 
   isSeparator(letter) {
-    for(let separator of Object.values(separators)) {
-      if(letter === separator) return true;
+    for (let separator of Object.values(separators)) {
+      if (letter === separator) return true;
     }
     return false;
   }
 
   getLiteralsType(word) {
-    if(Number.isFinite(Number(word))) {
+    if (Number.isFinite(Number(word))) {
       return literals.number;
-    } else if(word === "false" || word === "true") {
+    } else if (word === "false" || word === "true") {
       return literals.boolean;
-    } else if(word === "null") {
+    } else if (word === "null") {
       return literals.null;
     } else {
       return literals.string;
@@ -49,25 +56,29 @@ class Parser {
 
   joinLiterals() {
     let data = "";
-    const literalsJoinedArray = this.lexedData.map((letter, idx) => {
-      if(this.isSeparator(letter)) {
-        data = "";
-        return letter;
-      } else {
-        data += letter;
-        if(idx < this.lexedData.length - 1 && this.isSeparator(this.lexedData[idx + 1])) {
-          return data.trim();
+    const literalsJoinedArray = this.tokenizedData
+      .map((letter, idx) => {
+        if (this.isSeparator(letter)) {
+          data = "";
+          return letter;
+        } else {
+          data += letter;
+          if (
+            idx < this.tokenizedData.length - 1 &&
+            this.isSeparator(this.lexedData[idx + 1])
+          ) {
+            return data.trim();
+          }
         }
-      }
-    }).filter((letter) => {
-      return letter !== undefined;
-    });
+      })
+      .filter(letter => {
+        return letter !== undefined;
+      });
     return literalsJoinedArray;
   }
 }
 
 const str = "[123, 22, 33]";
 const parser = new Parser(str);
-parser.lexer();
-print(parser.lexedData);
 parser.tokenizer();
+parser.lexer();
