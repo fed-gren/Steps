70282f619f041902e1baf8232922b8016a1dc2c1
fed-gren <deveb22@gmail.com>
2019-04-30 10:42:56 +0900

    fix: exports ERR_MSG directly
    - ERR_MSG 따로 객체로 내보내지 않고 바로 exports 하도록 수정

--- JavaScript/Step05/javascript-todo-step5-3/oop/ErrorConstant.js
+++ JavaScript/Step05/javascript-todo-step5-3/oop/ErrorConstant.js
@@ -1,4 +1,4 @@
-const ERR_MSG = {
+module.exports = ERR_MSG = {
     "NO_SHELL" : "명령어에 $를 포함시켜주세요.",
     "INCORRECT_INST" : "사용가능한 명령어는 add | update | delete | show 입니다.",
     "PARAMETER_ERROR" : "해당 명령어의 매게변수가 잘못되었습니다.",
@@ -8,8 +8,4 @@ const ERR_MSG = {
     "TAG_SHAPE_ERROR" : "올바른 태그 형식이 아닙니다.",
     "NO_MORE_REDO" : "더 이상 redo 할 수 없습니다.",
     "NO_MORE_UNDO" : "더 이상 undo 할 수 없습니다.",
-}
-
-module.exports = {
-    ERR_MSG
 }
--- JavaScript/Step05/javascript-todo-step5-3/oop/app.js
+++ JavaScript/Step05/javascript-todo-step5-3/oop/app.js
@@ -1,50 +1,51 @@
-const readline = require("readline")
-const Todos = require("./Todos.js")
-const validator = require("./validator.js")
-const ERR_MSG = require("./ErrorConstant").ERR_MSG
+const readline = require("readline");
+const Todos = require("./Todos.js");
+const validator = require("./validator.js");
+const ERR_MSG = require("./ErrorConstant");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout
+});
 
-const todos = new Todos()
+const todos = new Todos();
 
-const getCommand = (command) => {
-    if (!(command === "undo" || command === "redo") && !validator.isContained(command, "$")) {
-        throw Error("NO_SHELL")
+const getCommand = command => {
+  if (
+    !(command === "undo" || command === "redo") &&
+    !validator.isContained(command, "$")
+  ) {
+    throw Error("NO_SHELL");
+  }
+  const [inst, ...rest] = command.split("$");
+  if (!validator.isCorrectCommand(inst)) {
+    throw Error("INCORRECT_INST");
+  }
+  return [inst, rest];
+};
+
+rl.setPrompt("명령어를 입력하세요 : ");
+rl.prompt();
+
+rl.on("line", async command => {
+  if (command === "quit") {
+    rl.close();
+  } else {
+    try {
+      [inst, params] = getCommand(command.trim());
+      const paramErrorFlag = params.some(el => {
+        return el === undefined && el === null && !el;
+      });
+      if (todos[inst].length !== params.length || paramErrorFlag) {
+        throw Error("PARAMETER_ERROR");
+      }
+
+      await todos[inst](...params);
+    } catch (e) {
+      console.log("\x1b[31m%s\x1b[0m", "Error : " + ERR_MSG[e.message]);
     }
-    const [inst, ...rest] = command.split("$")
-    if (!validator.isCorrectCommand(inst)) {
-        throw Error("INCORRECT_INST")
-    }
-    return [inst, rest]
-}
 
-rl.setPrompt("명령어를 입력하세요 : ")
-rl.prompt()
-
-rl.on("line", async (command) => {
-    if (command === "quit") {
-        rl.close()
-    } else {
-        try {
-            [inst, params] = getCommand(command.trim())
-            const paramErrorFlag = params.some((el) => {
-                return el === undefined && el === null && !el
-            })
-            if ((todos[inst].length !== params.length) || paramErrorFlag) {
-                throw Error("PARAMETER_ERROR")
-            }
-
-            await todos[inst](...params)
-        } catch (e) {
-            console.log('\x1b[31m%s\x1b[0m', "Error : " + ERR_MSG[e.message])
-        }
-
-        rl.prompt()
-
-
-    }
+    rl.prompt();
+  }
 }).on("close", () => {
-    process.exit()
-})
+  process.exit();
+});
